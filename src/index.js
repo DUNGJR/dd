f5968720055ea6e9a2f81f648dfd3fd92650761b
Dung <dunghoang879@gmail.com>
2023-06-25 22:41:38 +0700

    haha

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(morgan('combined'));
-
-app.engine('handlebars',engine());
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'));  
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
